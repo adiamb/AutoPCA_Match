0dea98b1c9a3acfc2e692ca7682157e1dc0e141f
Aditya Ambati <ambati@stanford.edu>
2019-05-15 12:52:39 -0700

    Finalized the prelim script

--- PCAmatch.R
+++ PCAmatch.R
@@ -1,9 +1,3 @@
-
-arg=commandArgs(trailingOnly = T)
-DF=fread(arg[1], header = T)# 
-names(DF)[1:3] = c('ID1', 'ID2', 'DX')
-Ncontrols = arg[2]
-Outfile = paste0(arg[3])
 PackList = rownames(installed.packages())
 ## check for reqd for packages else install if not found
 if('data.table' %in%  PackList & 'ggplot2' %in% PackList){
@@ -13,7 +7,11 @@ if('data.table' %in%  PackList & 'ggplot2' %in% PackList){
   install.packages('data.table')
   install.packages('ggplot2')
 }
-
+arg=commandArgs(trailingOnly = T)
+DF=fread(arg[1], header = T)# 
+names(DF)[1:4] = c('ID1', 'DX', 'C1', 'C2')
+Ncontrols = arg[2]
+Outfile = paste0(arg[3])
 ### this function should calculate the EUCL Distance expects a single query and many subjects
 Eucl.Dist=function(Query.DF, Subject.DF){
   dist.one=function(x1, x2){
@@ -32,18 +30,19 @@ Eucl.Dist=function(Query.DF, Subject.DF){
 }
 
 
+
 ## Run the function ##
 Main.Call = function(DF, Ncontrols){
-  DF.Cases=DF[DX == 1, -c(2:3)]
-  DF.Controls = DF[DX != 1, -c(2:3)]
+  DF.Cases=DF[DX == 1, -c(2)]
+  DF.Controls = DF[DX != 1, -c(2)]
   Case.Control.Master = NULL
   cat('Identified ', dim(DF.Cases)[1] , ' cases and ', dim(DF.Controls)[1])
   for( Subj in DF.Cases$ID1){
-    cat('Now Performing dist on ', Subj)
+    cat('Now Performing dist on ', Subj, sep = "\n")
     test.query=as.data.frame(DF.Cases[ID1 == Subj, -1])
     test.subject = as.data.frame(DF.Controls[,-1])
     out=Eucl.Dist(Query.DF = test.query, Subject.DF = test.subject)
-    dist.test=data.frame(IID = test.subject$ID1, dist=out)
+    dist.test=data.frame(IID = DF.Controls$ID1, dist=out)
     setDT(dist.test)
     dist.test=dist.test[order(dist)]
     dist.test$IID = as.character(dist.test$IID)
@@ -54,9 +53,17 @@ Main.Call = function(DF, Ncontrols){
   }
   return(Case.Control.Master)
 }
-
+### run the fucntion
+Case.Control.Master = Main.Call(DF=DF, Ncontrols = Ncontrols)
 ## Aggregate the results
 Case.Control.Master=apply(Case.Control.Master, 2, FUN=function(x) as.character(x))
 Case.Control.Master = as.data.frame(Case.Control.Master)
-Case.Control.Master
+setDT(Case.Control.Master)
+
+## create a plot of the true controls and cases
+DF$DX2 = ifelse(DF$ID1 %in%  unique(Case.Control.Master$CONTROLID), 'MATCHED.CONTROL', ifelse(DF$ID1 %in% unique(Case.Control.Master$CASEID), 'CASE', NA))
+p=ggplot(DF[!is.na(DX2)], aes(C1, C2, color = DX2))+geom_point(alpha=0.6)
+ggsave(p, filename = paste0(Outfile, ".png"), dpi = 400, device = "png", units = "in", width = 8, height = 5)
+## write the file to disk
+fwrite(DF, file = paste0(Outfile, ".csv"))
 
